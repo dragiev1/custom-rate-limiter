89f5960c046f14585259de31531168d5aa4d54ab
dragiev1 <dragievs2020@gmail.com>
2026-06-20 22:18:48 -0400

    added comments

--- src/ip-key-gen.ts
+++ src/ip-key-gen.ts
@@ -1,7 +1,13 @@
 import { isIPv6 } from 'node:net'
 import { Address6 } from 'ip-address'
 
-
+/**
+ * Returns IPv6/4 as a key to store for identification
+ * @param ip IP address
+ * 
+ * @param ipv6Subnet Subnet mask for IPv6 address 
+ * @returns {string}  generated key from IP address
+ */
 export function ipKeyGen(ip: string, ipv6Subnet: number | false = 56) {
   if(isIPv6(ip)) {
     const address = new Address6(ip)
