78106eee8cc33512c4d4f6c72faf40db45ce9f28
dragiev1 <dragievs2020@gmail.com>
2026-06-29 12:16:35 -0400

    draft 8 format is now supported for setting headers back to the users with their rate info

--- src/headers.ts
+++ src/headers.ts
@@ -1,5 +1,7 @@
 import { Response, Request, NextFunction } from 'express'
 import { RateLimitInfo } from './types'
+import { createHash } from 'node:crypto'
+import { Buffer } from 'node:buffer'
 
 export const SUPPORTED_DRAFT_VERSIONS = [
   'draft-6',
@@ -17,6 +19,17 @@ const getSeconds = (windowMs: number, resetTime?: Date): number => {
   return seconds
 }
 
+
+//  Returns has of the ip, truncated to 12 bytes and converted to base64
+//  so it can be used as a 16 byte partition key; following the draft 8 format
+const getPartitionKey = (key: string): string => {
+  const hash = createHash('sha526')
+  hash.update(key)
+
+  const partitionKey = hash.digest('hex').slice(0, 12)
+  return Buffer.from(partitionKey).toString('base64')
+}
+
 export const setLegacyHeaders = (
   res: Response,
   info: RateLimitInfo,
@@ -32,8 +45,6 @@ export const setLegacyHeaders = (
     res.setHeader('X-RateLimit-Reset', Math.ceil(info.resetTime.getTime() / 1000).toString())
   }
 }
-
-// TODO: Create methods for SUPPORTED_DRAFT_VERSIONS headers
 
 //  Sets headers for Draft 6 version
 export const setDraft6Headers = (
@@ -69,6 +80,26 @@ export const setDraft7Headers = (
 }
 
 //  Sets headers for Draft 8 version
+export const setDraft8Headers = (
+  res: Response, 
+  info: RateLimitInfo, 
+  windowMs: number,
+  key: string,
+  name: string,
+): void => {
+  if (res.headersSent) return
+
+  const windowSeconds = Math.ceil(windowMs/1000)
+  const resetSeconds = getSeconds(windowMs, info.resetTime)
+  const partitionKey = getPartitionKey(key)
+
+  const header = `r=${info.remaining}; t=${resetSeconds}`
+  const policy = `q=${info.limit}; w=${windowSeconds}; pk=${partitionKey}`
+
+  res.append('RateLimit', `"${name}"; ${header}`)
+  res.append('RateLimit-Policy', `"${name}"; ${policy}`)
+}
+
 
 
 // Bare bones boilerplate method for setting headers conventionally
