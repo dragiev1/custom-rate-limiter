c3753319b95e4930ce1659f9cf7ee19cf47db56c
dragiev1 <dragievs2020@gmail.com>
2026-07-05 21:07:39 -0400

    dictionary of validation checks made with a disable method within and a basic ip checking/error handling method

--- src/validations.ts
+++ src/validations.ts
@@ -30,6 +30,36 @@ const usedStores = new Set<Store>()
  * not used more than once per request.
  * The store can either be an instance (like MemoryStore where two instances do not share state)
  * or a string for stores where multiple instances typically share states (like a Redis store).
+ * Check documentation for more information. 
  */
 const singleCountKeys = new WeakMap<Request, Map<Store | string, string[]>>()
 
+
+// Dictonary of checks.
+
+const validations = {
+  enabled: {
+    default: true,
+  } as { [key: string]: boolean },
+ 
+  // Method for disabling validations
+  disable() {
+    for (const k of Object.keys(this.enabled)) this.enabled[k] = false
+  },
+
+  // Network related validation checks.
+  
+  ip(ip: string | undefined) {
+    if (ip === undefined) throw new ValidationError(
+      'custom-rate-limiter: UNDEFINED_IP_ADDRESS',
+      `An undefined 'request.ip' was detected. This might indicate misconfigurations or the connection was prematurely severed.`
+    )
+
+    if (!isIP(ip)) throw new ValidationError(
+      'custom-rate-limiter: INVALID_IP_ADDRESS',
+      `An invalid 'request.ip' was detected (${ip}).`
+    )
+  },
+
+  
+}
