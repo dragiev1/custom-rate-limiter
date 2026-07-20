30a3eb8f96267911c1fa5b1f7e3cf1ae9f1bc9f9
dragiev1 <dragievs2020@gmail.com>
2026-07-19 21:01:30 -0400

    exported key generator, memory store, defaults and all types to begin writing tests

--- src/app.ts
+++ src/app.ts
@@ -1,51 +1,9 @@
-/**
- *  Bare bones prototype
- */
+//  Export everything!
 
-import express from 'express';
-import type { Request, Response, NextFunction } from 'express';
+export { ipKeyGen } from './ip-key-gen.ts'
 
-const app = express();
-const PORT = 3000;
+export { MemoryStore } from './memory-store.ts'
 
-// Prototype map to hold store hit counts
-// Note: Key = IP addy, Value = number of hits
-const hitCounts = new Map<string, number>();
-const LIMIT = 5;
-const WINDOW_MS = 60000; // 1 min
+export { default, default as rateLimit } from './rate-limit.ts'
 
-// Custom middleware
-const simpleRateLimiter = (req: Request, res: Response, next: NextFunction) => {
-  // Identify user
-  const clientIP = req.ip || 'unknown';
-
-  // Get current hit count (default to 0 if they are new)
-  const currentHits = hitCounts.get(clientIP) || 0;
-
-  // Check if they exceeded limit
-  if(currentHits >= LIMIT) {
-    return res.status(429).json({
-      error: 'Too Many Requests',
-      message: `You have exceeded the limit of ${LIMIT} requests. Current hits: ${currentHits}.`
-    });
-  }
-
-  // Increment hit count and save back into map
-  hitCounts.set(clientIP, currentHits + 1);
-
-  // Allow the request to proceed to the actual route
-  next();
-};
-
-
-// Add middleware to all routes
-app.use(simpleRateLimiter);
-
-// Dummy apis to test rate limiter method
-app.get('/', (req: Request, res: Response) => {
-  res.json({message: 'Success. Not limited.'});
-});
-
-app.listen(PORT, () => {
-  console.log(`Server is running on http:localhost:${PORT}`);
-});
+export * from './types.ts'
